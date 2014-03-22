718800a4a03ed7f5802e3ec36f4b3180474728f7
Chetan Sarva <chetan@pixelcop.net>
2014-03-22 19:14:42 -0400

    require gdbm

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -28,7 +28,7 @@ dependency "libedit"
 dependency "openssl"
 dependency "libyaml"
 dependency "libiconv"
-dependency "gdbm" if (platform == "mac_os_x" or platform == "freebsd" or platform == "aix")
+dependency "gdbm" #if (platform == "mac_os_x" or platform == "freebsd" or platform == "aix")
 dependency "libgcc" if (platform == "solaris2" and Omnibus.config.solaris_compiler == "gcc")
 
 source :url => "http://cache.ruby-lang.org/pub/ruby/2.1/ruby-#{version}.tar.gz",
